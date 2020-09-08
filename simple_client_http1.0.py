c73819da91f4bc13a701276da5f95361fab5b244
Wei Chen <icecowoo@gmail.com>
2020-09-07 17:30:45 -0700

    upgraded to python 3

--- simple_client_http1.0.py
+++ simple_client_http1.0.py
@@ -2,7 +2,9 @@ from socket import *
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(("www.ucsd.edu", 80))
-s.send("GET / HTTP/1.0\r\n\r\n")
+s.send(b"GET / HTTP/1.0\r\n\r\n")
+# s.connect(("www.u.arizona.edu", 80))
+# s.send(b"GET /~weichen/MIS543/test.html HTTP/1.0\r\n\r\n")
 data = s.recv(1024)
-print data
+print(data)
 s.close()
--- simple_client_http1.1.py
+++ simple_client_http1.1.py
@@ -2,8 +2,8 @@ from socket import *
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(("www.ucsd.edu", 80))
-s.send("GET / HTTP/1.1\r\n" + 
-	"Host:www.ucsd.edu\r\n\r\n")
+get_req = "GET / HTTP/1.1\r\n" + "Host:www.ucsd.edu\r\n\r\n"
+s.send(get_req.encode())
 data = s.recv(1024)
-print data
+print(data)
 s.close()
